be79a0aa39ec83f6773d99f5e78adbe61487dd90
kimminki10 <pangshe10@gmail.com>
2024-08-31 20:48:35 +0900

    add youtube ojbect create

--- youtube/models.py
+++ youtube/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class YouTubeVideo(models.Model):
     video_id = models.CharField(max_length=50, unique=True)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True, blank=True)
-    duration = models.IntegerField()
-    thumbnail_url = models.URLField()
+    duration = models.IntegerField(null=True)
+    thumbnail_url = models.URLField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     summary = models.TextField(null=True, blank=True)
--- youtube/serializers.py
+++ youtube/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import YouTubeVideo
 
+from langserve import RemoteRunnable
+from pytube import YouTube
+
 class YouTubeVideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = YouTubeVideo
@@ -15,4 +18,25 @@ class YouTubeVideoSerializer(serializers.ModelSerializer):
             'updated_at',
             'summary',
         ]
-        read_only_fields = ['created_at', 'updated_at']
+        read_only_fields = ['title', 'description', 'duration', 'thumbnail_url', 'summary']
+
+    def create(self, validated_data):
+        video_id = validated_data.get('video_id')
+        additional_data = self.get_video_info(video_id)
+        validated_data.update(additional_data)
+        return super().create(validated_data)
+
+    def get_video_info(self, video_id):
+        youtube_url = f'https://www.youtube.com/watch?v={video_id}'
+        remote_chain = RemoteRunnable("http://localhost:8001/youtube-summary/")
+        
+        yt = YouTube(youtube_url)
+        summary = remote_chain.invoke(youtube_url)
+
+        return {
+            'title': yt.title,
+            'description': yt.description,
+            'duration': yt.length,
+            'thumbnail_url': yt.thumbnail_url,
+            'summary': summary
+        }
